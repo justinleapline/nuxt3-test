32a1135ce71f8f7626d380de18b19d83f7ad3fb1
Justin Leapline <justin@sdg.works>
2022-04-02 18:33:22 -0400

    init

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const client = useSupabaseClient();
+
+client.from("*").on("*", (payload) => {
+  console.log("Change received!", payload);
+});
+</script>
+
+<template>
+  <div></div>
+</template>
